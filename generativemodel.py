6de12c4b555509c844fdc6f7b7a10fa3d2a4f633
lucnietoX <luciano.nieto@gmail.com>
2024-06-24 19:26:49 +0100

    gemini ai with nvidia stockings analysis

--- generativemodel.py
+++ generativemodel.py
@@ -1,15 +1,40 @@
+import google.generativeai as genai
+import os
+import csv
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+from datetime import datetime
+import pandas as pd
+import io
+import base64
+from PIL import Image
+import PIL.Image
+
+#Configure the Google AI Vertex
+key=open("key.txt","r").readlines()[0]
+genai.configure(api_key=key)
 model = genai.GenerativeModel('gemini-1.5-flash')
 
-cookie_picture = {
-    'mime_type': 'image/png',
-    'data': pathlib.Path('cookie.png').read_bytes()
-}
-prompt = "Do these look store-bought or homemade?"
+#Import the data from NVIDIA Stock dataset
+data=pd.read_csv("dataset_nvidia_stocks.csv")
+data['Date'] = pd.to_datetime(data['Date'])
+data2 = data[data['Date'] >= '2020-01-01']
 
-response = model.generate_content(
-    model="gemini-1.5-flash",
-    content=[prompt, cookie_picture]
-)
+#Generate the trend chart
+plt.figure(figsize=(10, 5))
+plt.plot(data2['Date'], data2['Close'], marker='.', linestyle='-', color='#76b900')
+plt.title("NVIDIA Stocks Close price - since 2020 - daily")
+plt.xlabel("Dates")
+plt.ylabel("Price")
+plt.grid(axis='y', linestyle='--', alpha=0.7)
+plt.tight_layout()
+p = 'nvidia_chart_trend.png'
+plt.savefig(p)
+#Convert Image
+
+img = PIL.Image.open(p)
+#Ask to Gemini
+
+text = "Please, analyze and see what happened in the last 3 months? What was the contribution to increase NVIDIA price?"
+response = model.generate_content([text ,img])
 print(response.text)
-
-#reference: https://ai.google.dev/gemini-api/docs/api-overview
--- hellogemini.py
+++ hellogemini.py
@@ -2,8 +2,8 @@ import google.generativeai as genai
 import PIL.Image
 import os
 
-genai.configure(api_key=os.environ["GOOGLE_API_KEY"])
-img = PIL.Image.open('path/to/image.png')
+genai.configure(api_key="")
+img = PIL.Image.open('nvidia_chart_trend.png')
 
 model = genai.GenerativeModel(model_name="gemini-1.5-flash")
 response = model.generate_content(["What is in this photo?", img])
